93ca880408dd0adc137cba8d00f242e43d35d52d
Nik Hamidi <hamidi.hamat@live.iium.edu.my>
2020-06-24 22:41:05 +0800

    Add files via upload

---
+++ App.js
@@ -0,0 +1,61 @@
+import React, {Component} from 'react';
+import {Text, View, Button, TextInput, Alert} from 'react-native';
+import {Styles} from './Styles';
+
+export default class App extends Component{
+  constructor(){
+    super();
+    this.state = {
+
+      word: "",
+      vowel: 0,
+      consonant: 0,
+      calculate: 0
+      
+
+    }
+  }
+
+  calculation = () => {
+    let v = 0;
+    let k = 0; 
+    let word = this.state.word;
+
+    for(let i = 0 ; i < word.length ; ++i){
+      if ( word[i]=='a' || word[i]=='i' || word[i]=='o' || word[i]=='u' || word[i]=='e' ||  word[i]=='A' || word[i]=='I' || word[i]=='O' || word[i]=='U' || word[i]=='E')
+      {
+        v++;
+      }
+      else
+      {
+        k++;
+      }
+
+      this.setState({vowel:v})
+      this.setState({consonant:k});
+    }
+
+  }
+
+  render() {
+    return (
+      <View style={Styles.container}>
+        <View style={Styles.container}>
+        <Text style={Styles.title}>Word Analyser</Text>
+        <Text style={Styles.developer}>by Nik Hamidi</Text>
+        </View>
+        <View style={Styles.container}>
+        <TextInput onChangeText={(word) => this.setState({word})} style={Styles.instructions} placeholder='Type Your Word Here'/>
+       
+        </View>
+        <View style={Styles.container}>
+        <Button color="#ff7675" onPress={this.calculation} title='Analyse'/>
+        <Text></Text>
+        <Text style={Styles.answer}>Total Word : {this.state.word.length}</Text>
+        <Text style={Styles.answer}>Vowel: {this.state.vowel}</Text>
+        <Text style={Styles.answer}>Consonant: {this.state.consonant}</Text>
+        </View>
+      </View>
+    );
+  }
+}
---
+++ Styles.js
@@ -0,0 +1,32 @@
+import {StyleSheet} from 'react-native';
+
+export const Styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+      backgroundColor: '#003366',
+    },
+    
+    title: {
+      fontSize: 30,
+      textAlign: 'center',
+      margin: 10,
+      color: 'white'
+    },
+    developer:{
+      fontSize: 10,
+      textAlign: 'center',
+      color: 'white'
+    },
+    instructions: {
+      textAlign: 'center',
+      color: 'white',
+      marginBottom: 5,
+    },
+    answer:{
+      textAlign: 'center',
+      color: '#81ecec',
+      marginBottom: 5,
+    },
+  });
